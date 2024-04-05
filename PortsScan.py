2c10e4c1a2908a3a88afe9b0ef1fa2ce8867d543
Marcelofcdantas <marcelofcd@gmail.com>
2024-04-04 20:18:59 -0400

    upgraded

--- PortsScan.py
+++ PortsScan.py
@@ -2,9 +2,22 @@ import sys
 import socket
 import os.path
 
+def howmanyIPAdresses():
+    answer=input('How many IP addresses do you want to check the ports? ')
+    if answer < 1:
+        print('Invalid number')
+        howmanyIPAdresses()
+    else:
+        gettingDataAndScanningPorts(answer)
+        
 
-def gettingDataAndScanningPorts():
-    ipAddress=str(input('Which is the IP address of the machine to be scanned? \n'))
+def gettingDataAndScanningPorts(numberOfIPS):
+    if numberOfIPS == 1:
+        ipAddress=str(input('Which is the IP address of the machine to be scanned? \n'))
+    else:
+        firstIP=str(input('Which is the IP address of the first machine to be scanned? \n'))
+        lastIP=str(input('Which is the IP address of the last machine to be scanned? \n'))
+        ipInArray = dividingIPs(firstIP, lastIP)
     firstPort=int(input('Which is the first port to be scanned? \n'))
     lastPort=int(input('Which is the last port to be scanned? \n'))
     noPortsOpen = True
@@ -28,6 +41,68 @@ def gettingDataAndScanningPorts():
     print (f'All {portsNumber} ports scanned')
     generatingReport(listOfPorts)
 
+
+def dividingIPs(firstIP, lastIP):
+    IpPart = 1
+    for character in firstIP:
+        if (character != ".") and (IpPart == 1):
+            IP1ClassA += character
+        elif (character != ".") and (IpPart == 2):
+            IP1ClassB += character
+        elif (character != ".") and (IpPart == 3):
+            IP1ClassC += character
+        else:
+            IP1ClassC += character
+        IpPart +=1
+    ipInArray.append(IP1ClassA)
+    ipInArray.append(IP1ClassB)
+    ipInArray.append(IP1ClassC)
+    ipInArray.append(IP1ClassD)
+    IpPart = 1
+    for character in lastIP:
+        if (character != ".") and (IpPart == 1):
+            IP2ClassA += character
+        elif (character != ".") and (IpPart == 2):
+            IP2ClassB += character
+        elif (character != ".") and (IpPart == 3):
+            IP2ClassC += character
+        elif (character != ".") and (IpPart == 4):
+            IP2ClassC += character
+        IpPart +=1
+    ipInArray.append(IP2ClassA)
+    ipInArray.append(IP2ClassB)
+    ipInArray.append(IP2ClassC)
+    ipInArray.append(IP2ClassD)
+    listOfIps = creatingIpArrays(ipInArray)
+    return ipInArray
+
+
+def creatingIpArrays(ipInArray):
+    listOfIps = []
+    firstArray = 0
+    secondArray = 4
+    classAIP = separatingIpParts(ipInArray[firstArray], ipInArray[lastArray])
+    firstArray += 1
+    lastArray += 1
+    classBIP = separatingIpParts(ipInArray[firstArray], ipInArray[lastArray])
+    firstArray += 1
+    lastArray += 1
+    classCIP = separatingIpParts(ipInArray[firstArray], ipInArray[lastArray])
+    firstArray += 1
+    lastArray += 1
+    classDIP = separatingIpParts(ipInArray[firstArray], ipInArray[lastArray])
+    return generatingIps(classAIP, classBIP, classCIP, classDIP)
+
+def generatingIps(classAIP, classBIP, classCIP, classDIP):
+    
+
+def separatingIpParts(A, B):
+    while A <= B:
+        classIp.append(A)
+        A+= 1
+    return classIp
+
+
 def generatingReport(ports):
     for port in ports:
         try:
@@ -40,5 +115,6 @@ def generatingReport(ports):
                 f.write(f'{port} \n')
         except IOError:
             print("Path designated doesn't exist")
+
 
 gettingDataAndScanningPorts()
